8b9bb94e5d2cd143db0ac997aefcec65c53d606a
max <maxinlord777@gmail.com>
2025-06-03 12:35:10 +0300

    fix settings

--- bot/settings.py
+++ bot/settings.py
@@ -2,12 +2,15 @@ from pydantic import SecretStr
 from pydantic_settings import BaseSettings
 from redis.asyncio import Redis
 from sqlalchemy import URL
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-class SQLiteSettings(BaseSettings):
+class MysqlSettings(BaseSettings):
     db: str
-    username: str
     password: str
+    username: str
     host: str
 
 
@@ -22,16 +25,16 @@ class Settings(BaseSettings):
     dev: bool
     bot_token: SecretStr
 
-    psql: SQLiteSettings = SQLiteSettings(_env_prefix="MYSQL_")  # type: ignore
+    mysql: MysqlSettings = MysqlSettings(_env_prefix="MYSQL_")  # type: ignore
     redis: RedisSettings = RedisSettings(_env_prefix="REDIS_")  # type: ignore
 
     def sqlite_dsn(self) -> URL:
         return URL.create(
             drivername="mysql+aiomysql",
-            database=self.psql.db,
-            username=self.psql.username,
-            password=self.psql.password,
-            host=self.psql.host,
+            database=self.mysql.db,
+            username=self.mysql.username,
+            password=self.mysql.password,
+            host=self.mysql.host,
         )
 
     async def redis_dsn(self) -> Redis:
